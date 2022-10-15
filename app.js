d43886aba664e0f369d83b066586dede899648e7
Vansh <vanshayushi@gmail.com>
2022-10-15 21:43:52 +0530

    navbar bg change

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 // alert("hello")
 
 // navbar
-let list=document.getElementById('list')
-document.getElementById('nav').addEventListener('click',()=>{
-    if(list.style.height=="100%"){
-        list.style.height="0"
-    }
-    else{
-        list.style.height="100%";
-    }
-})
+let list = document.getElementById("list");
+document.getElementById("nav").addEventListener("click", () => {
+  if (list.style.height == "100%") {
+    list.style.height = "0";
+  } else {
+    list.style.height = "100%";
+  }
+});
+
+// navbar bg change
+const changebg = () => {
+  if (window.scrollY >= 40) {
+    document.getElementById("navbar").style.background = "#d7a85f";
+  } else {
+    document.getElementById("navbar").style.background = "transparent";
+  }
+};
+
+window.addEventListener("scroll", changebg);
